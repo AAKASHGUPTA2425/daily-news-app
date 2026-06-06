afe49d827843a252f186d3efe80a1d9f28224b78
Aakash Gupta <125762441+AAKASHGUPTA2425@users.noreply.github.com>
2026-06-06 14:57:36 +0530

    Switch from NewsAPI to GNews

--- src/api/newsApi.js
+++ src/api/newsApi.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
-const API_KEY = import.meta.env.VITE_NEWS_API_KEY;
+const API_KEY = import.meta.env.VITE_GNEWS_API_KEY;
 
 export const getNewsByCategory = async (category) => {
   let url;
 
   if (category === "national") {
-    url = `https://newsapi.org/v2/everything?q=India&sortBy=publishedAt&language=en&apiKey=${API_KEY}`;
+    url = `https://gnews.io/api/v4/top-headlines?country=in&apikey=${API_KEY}`;
   } else {
-    url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${API_KEY}`;
+    url = `https://gnews.io/api/v4/top-headlines?category=${category}&apikey=${API_KEY}`;
   }
 
   const response = await axios.get(url);
