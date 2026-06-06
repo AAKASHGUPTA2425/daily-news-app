dbfaf57f256e545f98d90fb80961cfba723e7c64
Aakash Gupta <125762441+AAKASHGUPTA2425@users.noreply.github.com>
2026-06-06 15:58:18 +0530

    Update API endpoint to /api/new

--- src/api/newsApi.js
+++ src/api/newsApi.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const getNewsByCategory = async (category) => {
   const response = await axios.get(
-    `/api/news?category=${category}`
+    `/api/new?category=${category}`
   );
 
   return response.data.articles;
